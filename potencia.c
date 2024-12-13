4bc4d4f47191b0d9993e1a4da065aea7a77b940d
danielsantos-ti <sysdsantos@gmail.com>
2024-12-12 22:11:51 -0300

    Primeiro commit

---
+++ potencia.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main()
+{
+
+    int a, b, pow, i;
+
+    printf("Digite um valor para a: ");
+    scanf("%d", &a);
+
+    printf("Digite um valor para b: ");
+    scanf("%d", &b);
+
+
+    for(i = 1; i < b; i++)
+    {
+        pow = pow + a;
+    }
+    
+    printf("%d", a);
+
+}
